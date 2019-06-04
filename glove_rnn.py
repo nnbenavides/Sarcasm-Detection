0b58cbbce97862d34797d69a29aa69e85b80eb74
Nicholas Benavides <12nbenavides@gmail.com>
2019-06-03 21:24:23 -0700

    added randomized hyperparameter search

--- glove_rnn.py
+++ glove_rnn.py
@@ -62,25 +62,6 @@ for i in range(len(test_indices)):
     index_map_test[i] = test_indices[i]
 print('size of test set:', str(len(index_map_test)))
 
-# Hyper-parameters
-sequence_length = 1
-input_size = 600
-hidden_size = 128
-num_layers = 2
-num_classes = 2
-batch_size = 100
-num_epochs = 2
-learning_rate = 0.003
-
-# Initialize data loaders
-train_loader = torch.utils.data.DataLoader(dataset=glove_train,
-                                           batch_size=batch_size, 
-                                           shuffle=True)
-
-dev_loader = torch.utils.data.DataLoader(dataset=glove_dev,
-                                          batch_size=batch_size, 
-                                          shuffle=False)
-
 # Bidirectional recurrent neural network (many-to-one)
 class BiRNN(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
@@ -101,49 +82,105 @@ class BiRNN(nn.Module):
         out = self.fc(out[-1, :, :])
         return out
 
-# Initialize model
-model = BiRNN(input_size, hidden_size, num_layers, num_classes)#.to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+# Model parameters
+sequence_length = 1
+input_size = 600
+num_classes = 2
 
-# Train the model
-total_step = len(train_loader)
-for epoch in range(num_epochs):
-    for i, (sequences, labels) in enumerate(train_loader):
-        
-        sequences = sequences.view(sequences.shape[0], input_size)#.to(device)
-        labels = labels#.to(device)
-        
-        # Forward pass
-        outputs = model(sequences)
-        loss = criterion(outputs, labels)
-        
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-        if (i+1) % 100 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+# Fixed Hyperparameters
+batch_size = 32
 
-# Save the model checkpoint
-torch.save(model.state_dict(), 'model.ckpt')
+# Hyperparameters to experiment with
+hidden_sizes = [100, 250, 500, 1000]
+num_layers = [1, 5, 10]
+num_epochs = [5, 10, 20]
+learning_rates = [0.1, 0.01, 0.001]
 
-# Format predictions so we can compute macro avg F1 score
-with torch.no_grad():
-    correct = 0
-    total = 0
-    preds = []
-    actuals = []
-    for sequences, labels in dev_loader:
-        sequences = sequences.view(sequences.shape[0], input_size)#.to(device)
-        labels = labels#.to(device)
-        outputs = model(sequences)
-        _, predicted = torch.max(outputs.data, 1)
-        preds.append(predicted.tolist())
-        actuals.append(labels.tolist())
+# Initialize data loaders
+train_loader = torch.utils.data.DataLoader(dataset=glove_train,
+                                           batch_size=batch_size, 
+                                           shuffle=True)
 
-preds = [item for sublist in preds for item in sublist]
-actuals = [item for sublist in actuals for item in sublist]
-print(classification_report(actuals, preds, digits=3))
+dev_loader = torch.utils.data.DataLoader(dataset=glove_dev,
+                                          batch_size=batch_size, 
+                                          shuffle=False)
+
+# initialize variables to track best model
+best_size = 0
+best_layers = 0
+best_epochs = 0
+best_eta = 0
+best_f1 = 0
+best_model = None
+
+np.random.seed(1738)
+for i in range(25):
+    # Randomly select parameters
+    hidden_size_ind = np.random.randint(0, len(hidden_sizes))
+    hidden_size = hidden_sizes[hidden_size_ind]
+    hidden_layer_ind = np.random.randint(0, len(num_layers))
+    layers = num_layers[hidden_layer_ind]
+    epochs_ind = np.random.randint(0, len(num_epochs))
+    epochs = num_epochs[epochs_ind]
+    lr_ind = np.random.randint(0, len(learning_rates))
+    learning_rate = learning_rates[lr_ind]
+
+    # Initialize model
+    model = BiRNN(input_size, hidden_size, layers, num_classes)#.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Train the model
+    total_step = len(train_loader)
+    for epoch in range(epochs):
+        for i, (sequences, labels) in enumerate(train_loader):
+            
+            sequences = sequences.view(sequences.shape[0], input_size)#.to(device)
+            labels = labels#.to(device)
+            
+            # Forward pass
+            outputs = model(sequences)
+            loss = criterion(outputs, labels)
+            
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+        print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, epochs, loss.item()))
+
+    # Format predictions so we can compute macro avg F1 score
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        preds = []
+        actuals = []
+        for sequences, labels in dev_loader:
+            sequences = sequences.view(sequences.shape[0], input_size)#.to(device)
+            labels = labels#.to(device)
+            outputs = model(sequences)
+            _, predicted = torch.max(outputs.data, 1)
+            preds.append(predicted.tolist())
+            actuals.append(labels.tolist())
+
+    preds = [item for sublist in preds for item in sublist]
+    actuals = [item for sublist in actuals for item in sublist]
+    report = classification_report(actuals, preds, digits=3, output_dict = True)
+    macro_f1 = report['macro avg']['f1-score']
+    print('\nfit model ', str(i), ' out of 25')
+
+    if macro_f1 > best_f1:
+        best_size = hidden_size
+        best_layers = layers
+        best_epochs = epochs
+        best_eta = learning_rate
+        best_f1 = macro_f1
+        best_model = model
+        print('\nupdated best parameters')
+
+print('Best F1: ',str(best_f1))
+print('Best hidden_size: ',str(best_size))
+print('Best num_layers: ', str(best_layers))
+print('Best num_epochs: ', str(best_epochs))
+print('Best learning_rate: ', str(best_eta))
+torch.save(best_model.state_dict(), 'model.ckpt')
