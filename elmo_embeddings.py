4f271231d4ee1ad8c0be4bc75212c9feef465e17
Ubuntu <ases-team@project-vm1.45yfhvz2l2uupp33ug0ztdwvve.bx.internal.cloudapp.net>
2020-02-27 09:24:01 +0000

    added print statements to track progress

--- elmo_embeddings.py
+++ elmo_embeddings.py
@@ -10,14 +10,16 @@ from allennlp.commands.elmo import ElmoEmbedder
 from collections import defaultdict
 import math
 
+print("starting elmo embedding script")
 # Read in Data
 data_dir = '../SARC/2.0/main'
 comments_file = os.path.join(data_dir, 'comments.json')
 train_file = os.path.join(data_dir, 'train-balanced.csv')
-
+print("read in data")
 with open(comments_file, 'r') as f:
     comments = json.load(f)
 
+print("starting to format data")
 # Format data
 train_ancestors = []
 train_responses = []
@@ -46,15 +48,16 @@ train_vocab = Counter(train_vocab)
 print(len(train_vocab))
 responses = train_responses
 print(len(responses))
-
+print("done formatting data")
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 elmo = ElmoEmbedder()
-
+print("starting to run batches of examples through the ElmoEmbedder")
 batch_size = 64
 num_batches = math.ceil(len(responses)/batch_size)
 all_example_embeddings = []
+print('num batches', num_batches)
 for i in range(num_batches):
     batch_examples = []
     if i % 10 == 0:
@@ -70,8 +73,8 @@ for i in range(num_batches):
     first_sentences = [nltk.word_tokenize(x[0]) for x in batch_examples]
     second_sentences = [nltk.word_tokenize(x[1]) for x in batch_examples]
     
-    first_batch = elmo.embed_batch(first_sentences[:64])
-    second_batch = elmo.embed_batch(second_sentences[:64])
+    first_batch = elmo.embed_batch(first_sentences)
+    second_batch = elmo.embed_batch(second_sentences)
     for j, sent in enumerate(first_batch):
         first_sent_embedding = np.mean(sent[2], axis = 0)
         first_sentence_embeddings.append(list(first_sent_embedding))
@@ -79,8 +82,11 @@ for i in range(num_batches):
         second_sentence_embeddings.append(list(second_sent_embedding))
     example_embeddings = [first_sentence_embeddings[k] + second_sentence_embeddings[k] for k in range(len(first_sentence_embeddings))]
     all_example_embeddings += example_embeddings
-
+print("starting to save elmo-X file")
 np.save('balanced-elmo-X.npy', all_example_embeddings)
+print("saved elmo-X file")
 labels = [int(x[0]) for x in train_labels]
+print("starting to save elmo-Y file")
 np.save('balanced-elmo-Y.npy', labels)
+print("saved elmo-Y file")
 
